7f4872c6dd449630fc4d09cefee4da0eb4737cbe
Simon Joyce <sj.malice+git@gmail.com>
2018-07-23 17:29:24 -0400

    separated graphs and added user input to select users

--- server.R
+++ server.R
@@ -19,61 +19,82 @@ data$Users = gsub("Commerical", "Commercial", data$Users)
 data$Users = gsub("Gov/Mil", "Government/Military", data$Users)
 #format date column
 data$`Date of Launch` = parse_date(data$`Date of Launch`, '%m/%d/%Y')
+#add year column for filtering
+data = data %>%
+  mutate(year = as.integer(substr(`Date of Launch`,1, 4)))
+
+#find positions of users
+User_pos = list(
+  civ_pos = grep("Civil", data$Users),
+  com_pos = grep("Commercial", data$Users),
+  mil_pos = grep("Military", data$Users),
+  gov_pos = grep("Government", data$Users)
+)
+
+purpose_categories = c("Communications",
+                       "Earth Observation",
+                       "Technology Development",
+                       "Navigation",
+                       "Space Science",
+                       "Earth Science",
+                       "Technology Demonstration",
+                       "Space Observation")
+
+#functions to find list of unique instances within a column separated by /
+list_sep = function(column){
+  column %>% 
+    strsplit('/') %>% 
+    unlist() %>% 
+    unique()
+}
+
+#function to build data frames for gvis
+
+fill_table = function(list, column){
+  d_f = data.frame(
+    names = list
+  )
+  
+  d_f = d_f %>% 
+    mutate(
+      pos = sapply(list, function(x){
+        grep(x, column)
+      }),
+      Total = lengths(pos),
+      Civil = sapply(pos, function(x){
+        length(intersect(x, User_pos$civ_pos))
+      }),
+      Commercial = sapply(pos, function(x){
+        length(intersect(x, User_pos$com_pos))
+      }),
+      Military = sapply(pos, function(x){
+        length(intersect(x, User_pos$mil_pos))
+      }),
+      Government = sapply(pos, function(x){
+        length(intersect(x, User_pos$gov_pos))
+      })
+    )
+}
 
 shinyServer(function(input, output) {
+  #filter satellites by users selected
+  user_slice = reactive({
+    sort(
+      unique(
+        unlist(User_pos[as.integer(input$users)], use.names = F)
+      ))
+  })
+  #filter satelites by date input and selected users
+  temp = reactive({
+    data[user_slice(),] %>%
+      filter(between(year, input$dates[1], input$dates[2]))
+  })
+  
   output$map <- renderGvis({
     
-    #filter satelites by date input
-    temp = 
-      data %>%
-      mutate(year = as.integer(substr(`Date of Launch`,1, 4))) %>%
-      filter(between(year, input$dates[1], input$dates[2]))
-    
-    #functions to find list of unique instances within a column separated by /
-    list_sep = function(column){
-      column %>% 
-        strsplit('/') %>% 
-        unlist() %>% 
-        unique()
-    }
-    
-    #find positions of users
-    civ_pos = grep("Civil", temp$Users)
-    com_pos = grep("Commercial", temp$Users)
-    mil_pos = grep("Military", temp$Users)
-    gov_pos = grep("Government", temp$Users)
-    
-    #Build data frame for map
-    
-    fill_table = function(list, column){
-      d_f = data.frame(
-        names = list
-      )
-      
-      d_f = d_f %>% 
-        mutate(
-          pos = sapply(list, function(x){
-                                 grep(x, column)
-                               }),
-          Total = lengths(pos),
-          Civil = sapply(pos, function(x){
-            length(intersect(x, civ_pos))
-          }),
-          Commercial = sapply(pos, function(x){
-            length(intersect(x, com_pos))
-          }),
-          Military = sapply(pos, function(x){
-            length(intersect(x, mil_pos))
-          }),
-          Government = sapply(pos, function(x){
-            length(intersect(x, gov_pos))
-          })
-        )
-    }
-    
     data_map = fill_table(
-      list_sep(temp$`Country of Operator/Owner`),
-      temp$`Country of Operator/Owner`
+      list_sep(temp()$`Country of Operator/Owner`),
+      temp()$`Country of Operator/Owner`
     )
     
     map_options = list(
@@ -85,42 +106,31 @@ shinyServer(function(input, output) {
       width="auto",
       height="auto")
     
-    world = data_map %>% 
+    data_map %>% 
       gvisGeoChart(locationvar = "names",
                    colorvar = 'Total',
                    # hovervar = "Military",
                    options = map_options
         )
-    
+  })
+  output$bar = renderGvis({
     #data frame for bar chart
-    purposes_left = list_sep(temp$Purpose)
-    
-    purpose_categories = c("Communications",
-                           "Earth Observation",
-                           "Technology Development",
-                           "Navigation",
-                           "Space Science",
-                           "Earth Science",
-                           "Technology Demonstration",
-                           "Space Observation")
-    
     data_bar = fill_table(
-      intersect(purposes_left, purpose_categories),
-      temp$Purpose
+      intersect(list_sep(temp()$Purpose), purpose_categories),
+      temp()$Purpose
     )
-    
-    bar = data_bar %>% 
-      arrange(desc(Total)) %>% 
+
+    data_bar %>%
+      arrange(desc(Total)) %>%
       gvisBarChart(
         xvar = "names",
         yvar = c("Civil", "Commercial", "Military", "Government"),
         options=list(title="Purposes and Users",
                      isStacked = TRUE,
-                     legend="bottom")
+                     legend="bottom",
+                     width="auto",
+                     height="auto")
       )
-    
-    gvisMerge(world, bar, horizontal = T)
-  
   })
 })
 
--- ui.R
+++ ui.R
@@ -15,26 +15,40 @@ shinyUI(dashboardPage(
   dashboardSidebar(
     sidebarUserPanel("Simon Joyce"),
     sidebarMenu(
-      menuItem("Map", tabName = 'map', icon = icon('map'))
-    )
+      menuItem("Map", tabName = 'map', icon = icon('map')),
+      menuItem("Purposes", tabName = 'bar', icon = icon('bar'))
+      ),
+    fluidRow(
+        sliderInput(
+          "dates",
+          label = h3("Launch Date Range"),
+          min = 1974, 
+          max = 2016,
+          value = c(1974, 2016)
+          )
+      ),
+    fluidRow(
+      checkboxGroupInput("users", label = h3("Users"), 
+                         choices = list("Civil" = 1,
+                                        "Commercial" = 2,
+                                        "Military" = 3,
+                                        "Government" = 4
+                                        ),
+                         selected = c(1, 2, 3, 4)
+      )
+      )
     ),
   dashboardBody(
     tabItems(
       tabItem(tabName = "map",
               fluidRow(
-                box(
-                  sliderInput(
-                    "dates",
-                    label = h3("Launch Date Range"),
-                    min = 1974, 
-                    max = 2016,
-                    value = c(1974, 2016)
-                  ) 
+                box(htmlOutput("map"), width = 12)
                 )
-              ),
+      ),
+      tabItem(tabName = "bar",
               fluidRow(
-                box(htmlOutput("map"))
-                )
+                box(htmlOutput("bar"), width = 12)
+              )
       )
     )
   )
